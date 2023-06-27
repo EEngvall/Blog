84bf17d97cfb098cd742e78c75941f01b8254497
Erik Engvall <erik.engvall@snhu.edu>
2023-06-27 12:37:35 -0700

    Made changes to new article creation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
 
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
@@ -12,19 +13,8 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "Test description 2",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: articles });
 });
 
