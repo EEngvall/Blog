bde3899153e8820fc415079cf6b082f101672a38
Erik Engvall <erik.engvall@snhu.edu>
2023-06-27 17:59:05 -0700

    Updated DB connection for Atlas cluster

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
@@ -5,10 +7,31 @@ const app = express();
 const articleRouter = require("./routes/articles");
 const Article = require("./models/article");
 
-mongoose.connect("mongodb://localhost/blog", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const cluster = "cluster0.frqsy";
+const dbname = "blog";
+
+mongoose.connect(
+  `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
 });
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.connect("mongodb://localhost/blog", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 app.set("view engine", "ejs");
 
@@ -21,4 +44,6 @@ app.get("/", async (req, res) => {
 });
 
 app.use("/articles", articleRouter);
-app.listen(5000);
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
